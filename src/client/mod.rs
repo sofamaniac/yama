79b0ee6ec5575f2c2a9c6f10a4ba786393cb09bb
Antoine Grimod <antoine.grimod@gmail.com>
2024-08-26 11:55:25 +0200

    setup workspace

--- src/client/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod interface;
-#[cfg(feature = "mpv")]
-mod mpv;
-#[cfg(feature = "local")]
-pub mod local;
-#[cfg(feature = "spotify")]
-pub mod spotify;
-#[cfg(feature = "youtube")]
-pub mod youtube;
--- src/client/youtube/mod.rs
+++
@@ -1,5 +0,0 @@
-mod backend;
-pub use backend::*;
-
-mod handler;
-pub use handler::Client;
